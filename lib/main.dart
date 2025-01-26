c2859e5374a0f106756755e4b459b24220bdd1a3
elifnurdemir <elifxnur118@gmail.com>
2025-01-26 19:43:59 +0300

    İlk commit: Flutter projesi

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+void main() {
+  int age = 24;
+  String name = "Elifnur";
+  double height = 1.64;
+  bool isStudent = true;
+  dynamic favoriteColor = "Green";
+  var initial = 'E';
+
+  print("Int: $age");
+  print("String: $name");
+  print("Double: $height");
+  print("Bool: $isStudent");
+  print("Dynamic: $favoriteColor");
+  print("Char: $initial");
+
+  int myAge = 24;
+  int my_age = 24;
+  int MyAge = 24;
+
+  print("CamelCase: $myAge");
+  print("snake_case: $my_age");
+  print("PascalCase: $MyAge");
+
+  String myName = "Elifnur";
+  String mySurname = "Demir";
+  int myAge2 = 24;
+  bool isAdult = true;
+
+  print("Adım: $myName");
+  print("Soyadım: $mySurname");
+  print("Yaşım: $myAge2");
+  print("Reşit miyim? $isAdult");
+}
